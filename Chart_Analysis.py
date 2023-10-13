210a3d593f109aa7a357de80ebf00564b1b36fbb
Drake Davis <drakedavis0203@gmail.com>
2023-10-12 21:19:12 -0400

    outline in pseudo-code

---
+++ Chart_Analysis.py
@@ -0,0 +1,17 @@
+# Import necessary libraries
+
+# Load dataset
+
+# Preprocess data
+
+# Split data into train and test sets
+
+# Convert arrays to PyTorch tensors
+
+# Define the neural network architecture
+
+# Initialize model, criterion, and optimizer
+
+# Train the model
+
+# Evaluate model
