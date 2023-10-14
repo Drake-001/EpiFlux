73472fc25a1cb2e2d41a02a1d273f1c056819d44
Drake Davis <drakedavis0203@gmail.com>
2023-10-14 14:55:33 -0400

    pseudo-code, high level, more detailed

---
+++ EpiFlux Code.py
@@ -0,0 +1,32 @@
+# BEGIN EpiFlux
+
+#   IMPORT necessary PyTorch libraries
+  
+#   DEFINE dataset 
+#     # This could be a collection of statements, facts, or experiences that the primary network uses to form beliefs
+  
+#   DEFINE primary_network 
+#     # This network will take input data and produce some form of belief or knowledge representation
+  
+#   DEFINE secondary_network
+#     # This network will take the outputs (beliefs) of the primary network and evaluate/reflect upon them
+
+#   DEFINE training_loop
+#     FOR each epoch
+#       FOR each data_point in dataset
+#         primary_belief = primary_network(data_point)
+#         evaluation = secondary_network(primary_belief)
+        
+#         CALCULATE loss using the evaluation and desired outcomes
+#         BACKPROPAGATE through both networks to optimize weights
+
+#   EXECUTE training_loop
+
+#   EVALUATE final beliefs and evaluations
+#     # Assess how primary network's beliefs have stabilized and what the secondary network's reflections reveal about them
+
+#   DRAW insights 
+#     # Analyze and interpret results in context of Rorty’s philosophical ideas and implications for AI model interpretability
+
+# END EpiFlux
+
