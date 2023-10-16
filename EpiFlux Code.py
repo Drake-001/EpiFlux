f71674d24598976065e7084ecc3f4aab80f0fea8
Drake Davis <drakedavis0203@gmail.com>
2023-10-15 21:30:43 -0400

    Training step 1 with ResNet18

--- EpiFlux Code.py
+++ EpiFlux Code.py
@@ -1,5 +1,17 @@
 
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
+data = torch.rand(1, 3, 64, 64)
+labels = torch.rand(1, 1000)
 
+# forward pass
+prediction = model(data)
+loss = (prediction - labels).sum()
+loss.backward() #backward pass
+
+optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+optim.step() #gradient descent
 a = [1.0, 2.0, 1.0]
 a = [0]
 
